f2c3ca9cbdb6b1b0558e078087eba47611d5e5f9
Sarthak-Gupta <Sarthak.Gupta@coxautoinc.com>
2025-09-14 06:22:08 +0530

    Props

--- react-project/src/App.js
+++ react-project/src/App.js
@@ -4,8 +4,6 @@ function App() {
   return (
     <>
     <React/>
-    {/* <a href="http://google.com" target="_blank" rel="noopener noreferrer">Google</a>
-   <h1>hello sarthak </h1> */}
     </>
   );
 }
